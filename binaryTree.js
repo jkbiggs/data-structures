ff6b73d2fe4afc702cfc947c3bb5df214e13a825
Joshua Biggs <joshua.biggsk@gmail.com>
2019-01-08 10:48:29 -0800

    exploring binary tree traversals

--- binaryTree.js
+++ binaryTree.js
@@ -19,17 +19,60 @@ function createBinaryNode(key) {
         }
     }
 }
-
-const TRAVERSAL = {
-    IN_ORDER:
-    PRE_ORDER:
-    POST_ORDER:
+// use enumerables for the traversal methods
+const TRAVERSALS = {
+    IN_ORDER: (node, visitFn) => {
+        if (node != null) {
+            TRAVERSALS.IN_ORDER(node.left, visitFn)
+            visitFn(node)
+            TRAVERSALS.IN_ORDER(node.right, visitFn)
+        }
+    },
+    PRE_ORDER: (node, visitFn) => {
+        if (node != null) {
+            visitFn(node)
+            TRAVERSALS.PRE_ORDER(node.left, visitFn)
+            TRAVERSALS.PRE_ORDER(node.right, visitFn)
+        }
+    },
+    POST_ORDER: (node, visitFn) => {
+        if (node != null) {
+            TRAVERSALS.POST_ORDER(node.left, visitFn)
+            TRAVERSALS.POST_ORDER(node.right, visitFn)
+            visitFn(node)
+        }
+    },
 }
 
 function createBinaryTree(rootKey) {
     const root = createBinaryNode(rootKey)
 
     return { 
-        root
+        root,
+        print(traversalType = 'IN_ORDER') {
+            let result = ''
+
+            const visit = node => {
+                result += result.length === 0 ? node.key : ` => ${node.key}`
+            }
+            
+            TRAVERSALS[traversalType](this.root, visit)
+
+            return result
+        }
     }
 }
+
+const tree = createBinaryTree('a')
+const b = tree.root.addLeft('b')
+const c = tree.root.addRight('c')
+const d = b.addLeft('d')
+const e = b.addRight('e')
+const f = c.addLeft('f')
+const g = c.addRight('g')
+const h = d.addLeft('h')
+const i = d.addRight('i')
+
+console.log(tree.print())
+console.log(tree.print('PRE_ORDER'))
+console.log(tree.print('POST_ORDER'))
